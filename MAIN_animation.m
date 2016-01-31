8ad5cd4b35991cad74d7a8640978ec33a8e88ef7
Matthew Kelly <matthew.kelly2@gmail.com>
2016-01-31 18:39:46 -0500

    Added code to save animation to a file

--- MAIN_animation.m
+++ MAIN_animation.m
@@ -1,11 +1,12 @@
-% MAIN.m  --  Animation of the Driven Damped Pendulum Fractal 
+% MAIN.m  --  Animation of the Driven Damped Pendulum Fractal
 %
 %
 clc; clear;
+flagSaveAnimation = false;
 
 % Specify the initial search grid
-q = linspace(-pi,pi,250);  %Angles
-w = linspace(-5,5,180);  %Rates
+q = linspace(-pi,pi,150);  %Angles
+w = linspace(-5,5,80);  %Rates
 
 % Expand out to a grid:
 [qq, ww] = meshgrid(q,w);
@@ -19,24 +20,45 @@ dynSys = @(t,z)( [z(2,:);  cos(t) - 0.1*z(2,:) - sin(z(1,:))] );
 
 % Numerical integration of system:
 dt = 0.02;
-tSpan = [0, 50];
+tSpan = [0, 2*pi*15];
 nData = ceil((tSpan(2)-tSpan(1))/dt);
 t = linspace(tSpan(1), tSpan(2),nData);
 Z = rk4(dynSys,t,Z0);
 
+if flagSaveAnimation
+% Set up stuff for writing animation to file:
+vidObj = VideoWriter('DrivenDampedPendulum.mp4','MPEG-4');   %#ok<*UNRCH> %This doesn't work on Linux   :-(   windows only
+vidObj.Quality = 100;
+open(vidObj);
+end
+
 % Plot a few of the frames:
 hFig = figure(1); clf; hFig.Color = 0.1*[1,1,1];
-idxPlot = 1:2:nData;
-dotSize = 6*ones(size(E0));
+axis([-4*pi,4*pi,-3,3]); axis off;
+idxPlot = 1:1:nData;
+dotSize = 15*ones(size(E0));
 dotColor = sqrt(E0-min(E0));  %Scale to make nice colors
 for i=idxPlot
     qPlot = Z(1,:,i);
     wPlot = Z(2,:,i);
-    scatter(qPlot,wPlot,dotSize,dotColor,'.');
+    if i==1
+        hDots = scatter(qPlot,wPlot,dotSize,dotColor,'.');
+    else
+        set(hDots,'XData',qPlot,'YData',wPlot);
+    end
     axis([-3*pi,3*pi,-3,3]); axis off;
-    pause(0.04);
+    set(gca,'Clipping','off');
+    drawnow;
+    
+    if flagSaveAnimation
+    % Save the frame:
+    writeVideo(vidObj,getframe(hFig));
+    end
 end
 
+if flagSaveAnimation
+close(vidObj);
+end
 
 
 
